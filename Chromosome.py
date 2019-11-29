37140c343db90eb648ebd2bd3fcb6a05c08a458c
ali risheh <a_icam@yahoo.com>
2019-11-29 19:27:41 +0330

    chromosome with vector of genes and crossover completed

--- Chromosome.py
+++ Chromosome.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 class Chromosome:
     GENESNUMBER = 100
     MIN = 0
@@ -7,12 +8,16 @@ class Chromosome:
         self.GENESNUMBER = chromosome_length
         self.MIN = min
         self.MAX = max
-        # Todo create a random list for genes between min and max below
+        # gene[0] = a and gene[1] = b
         self.genes = [[random.randint(self.MIN,self.MAX), random.randint(self.MIN,self.MAX)] for i in range(self.GENESNUMBER)]
         self.score = [0 for i in range(self.GENESNUMBER)]
 
-    def evaluate(self):
-        """
-        Update Score Field Here
-        """
-        #Todo
+    def evaluate(self, input_dots):
+        fitness_vector = []
+        for gene in self.genes:
+            gene_output = []
+            for input_dot in input_dots:
+                gene_output.append(input_dot[0]*gene[0] + input_dot[1]*gene[1])
+            # use standard deviation as fitness
+            fitness_vector.append(np.std(gene_output))
+        return fitness_vector
--- ES.py
+++ ES.py
@@ -1,8 +1,10 @@
+import random
+import numpy as np
 Mu = 10
 # Todo change Mu's coefficient below to attain the best result
 Lambda = 1*Mu
 crossover_probability = 0.4
-
+mutation_probability = .02
 
 def generate_initial_population():
     # Todo
@@ -10,16 +12,31 @@ def generate_initial_population():
 
     return list_of_chromosomes
 
-def generate_new_seed():
-    """
-    :return: return lambda selected parents
-    """
-    #Todo
-    return
-
-def crossover(chromosome1, chromosome2):
-    # Todo
-    return
+def crossover(chromosome, input_dots, sigma, remove_rate):
+    fitness_vector = chromosome.evaluate(chromosome, input_dots)
+    # first we combine fitness and gene vector (a,b)
+    fitness_gene = []
+    for i in range(len(chromosome.genes)) :
+        fitness_gene.append({"fitness": fitness_vector[0], "gene": chromosome.genes[i], "index": i})
+    fitness_gene_sorted = sorted(fitness_gene, key= lambda i: i['fitness'], reverse=True)
+    parent_genes = []
+    # we choose parents by a chance
+    for i in range(Lambda):
+        if random.randint(0,100) < 100 * crossover_probability :
+            parent_genes.append(fitness_gene_sorted[i]["gene"])
+    # crossover
+    children = []
+    for i in range (len(parent_genes) - 1):
+        # mutation with normal distribution
+        mutation_a = np.random.normal(0, sigma, 1)[0] if random.randint(0, 1000) < mutation_probability else 0
+        mutation_b = np.random.normal(0, sigma, 1)[0] if random.randint(0, 1000) < mutation_probability else 0
+        children.append([(parent_genes[i][0]*.6 + parent_genes[i + 1][0]*.4) + mutation_a,
+                         (parent_genes[i][1]*.6 + parent_genes[i + 1][1]*.4) + mutation_b])
+    chromosome.genes.append(children)
+    # remove bad genes
+    for i in range(remove_rate):
+        del chromosome.genes[fitness_gene_sorted[len(fitness_gene_sorted) - i]["index"]]
+    return chromosome
 
 def mutation(chromosome):
     """
